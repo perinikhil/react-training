d5bad03d00c4d7998a989e1bb63878b11a8b5927
Dmitry Belyaev <dmitry.belyaev@booking.com>
2019-10-20 17:07:44 +0200

    Reset to beginner start

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HotelCard from './components/HotelCard/HotelCard';
-import './index.css';
 
-function App() {
-  return (
-    <div className="container">
-      <HotelCard />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// Hey there! Let's start here :)
